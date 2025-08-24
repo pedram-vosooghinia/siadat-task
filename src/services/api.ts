df9d0cd8b1f77d5985aebd2f576cc9e59c881d05
Pedram <p.vosooghinia69@gmail.com>
2025-08-24 11:42:35 +0330

    fix: protection url in env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,26 +1,16 @@
 import axios, { AxiosError } from "axios";
 import { toast } from "react-hot-toast";
 
-
-const API_BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://siadat-task.vercel.app/api/"
-    : "http://localhost:3000/api/";
-
-
-
 export const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: { "Content-Type": "application/json" },
 });
-
-
 
 const errorInterceptor = async (axiosError: AxiosError) => {
   if (axiosError.response) {
     toast.error("Server error");
   } else if (axiosError.request) {
-    toast.error("Problem connecting to the server" );
+    toast.error("Problem connecting to the server");
   } else {
     toast.error("Unknown error");
   }
